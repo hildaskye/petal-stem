129461b6a67e17e74d003f8e029a994e78a78fc6
Cindy Lam <cindythilam@gmail.com>
2023-07-18 22:27:48 +0000

    Add personal plant

---
+++ api/routers/add_plant.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Depends
+from typing import Union, List
+from queries.add_plant import (
+    Error,
+    PlantIn,
+    PlantOut,
+    PlantRepository
+)
+
+router = APIRouter()
+
+@router.post("/api/garden/{user_id}/plant", response_model=Union[PlantOut, Error])
+def add_species(
+    plant: PlantIn,
+    repo: PlantRepository = Depends()
+):
+    return repo.create(plant)
