83077ef94fab0a99a10ddce29e7bb4e5f6046c30
Alan Cheng <alancheng@berkeley.edu>
2023-07-25 19:28:04 +0000

    Pest detail

--- api/routers/pest_detail.py
+++ api/routers/pest_detail.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from typing import Union, List
+from authenticator import authenticator
 from queries.pest_detail import (
     Error,
     PestDetailOut,
@@ -11,6 +12,7 @@ router = APIRouter()
 @router.get("/api/pest/{pest_id}", response_model=Union[List[PestDetailOut], Error])
 def get_pest_detail(
     pest_id: int,
-    repo: PestDetailRepository = Depends()
+    repo: PestDetailRepository = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
 ):
     return repo.get_pest_detail(pest_id)
