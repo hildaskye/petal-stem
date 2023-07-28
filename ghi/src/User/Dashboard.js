72de2f6bf179b8fb5c32822ae6e3998b32fa2bb4
Dylan Winn <dylanwinn09@gmail.com>
2023-07-28 17:49:44 +0000

    Generalcleanup

--- ghi/src/User/Dashboard.js
+++ ghi/src/User/Dashboard.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
 import useToken from "../auth forms/newindex.tsx";
+import { useNavigate } from "react-router-dom";
 
 export default function Dashboard() {
   const [plants, setPlants] = useState([]);
   const { user_id } = useParams();
   const { token } = useToken();
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function getData () {
@@ -25,9 +27,29 @@ export default function Dashboard() {
         setPlants(data);
       }
     };
-
     getData();
   }, [user_id, token]);
+
+  const handleDelete = async (plantId) => {
+    const url = `${process.env.REACT_APP_API_HOST}/api/garden/${user_id}/plant/${plantId}`;
+    const fetchConfig = {
+      method: "delete",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await fetch(url, fetchConfig);
+    if (response.ok) {
+      setPlants((prevPlant) =>
+        prevPlant.filter((plant) => plant.id !== plantId)
+      );
+    }
+  };
+
+  const handleEdit = (plantId) => {
+    navigate(`/garden/${user_id}/plant/${plantId}/edit`);
+  };
 
 
   return (
@@ -48,6 +70,20 @@ export default function Dashboard() {
                   <tr key={personal_plant.id}>
                     <td>{personal_plant.nickname}</td>
                     <td>{personal_plant.picture}</td>
+                    <td>
+                      <button
+                        className="btn btn-warning"
+                        onClick={() => handleEdit(personal_plant.id)}
+                      >
+                        Edit
+                      </button>
+                      <button
+                        className="btn btn-danger"
+                        onClick={() => handleDelete(personal_plant.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
                   </tr>
                 );
               })}
